6120ff32b660b523ea6dc174e08c7f6391b81095
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-08 20:07:55 +0800

    Update douyin-aweme.js

--- Quantumult/X/js/douyin-aweme.js
+++ Quantumult/X/js/douyin-aweme.js
@@ -5,17 +5,6 @@ if (obj.aweme_list) {
           obj.aweme_list[i].is_ads == true) {
       obj.aweme_list.splice(i, 1);
     }
-    if (obj.aweme_list[i].poi_info) {
-      delete obj.aweme_list[i].poi_info;
-    }
-    if (obj.aweme_list[i].sticker_detail) {
-      delete obj.aweme_list[i].sticker_detail;
-    }
-    if (obj.aweme_list[i].simple_promotions) {
-      delete obj.aweme_list[i].simple_promotions;
-    }
-    obj.aweme_list[i].status.reviewed = 1;
-    obj.aweme_list[i].video_control = arr;
   }
 }
 $done(JSON.stringify(obj));
