fc24a066d7b0426becd1519fb731b0336a1f3681
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-08 20:05:28 +0800

    Update douyin-aweme.js

--- Quantumult/X/js/douyin-aweme.js
+++ Quantumult/X/js/douyin-aweme.js
@@ -1,15 +1,4 @@
-let arr = {
-  "allow_download": true,
-  "share_type": 0,
-  "show_progress_bar": 0,
-  "draft_progress_bar": 0,
-  "allow_duet": true,
-  "allow_react": true,
-  "prevent_download_type": 2,
-  "allow_dynamic_wallpaper": false
-};
-let body = $response.body.replace(/watermark=1/g, "watermark=0");
-var obj = JSON.parse(body);
+var obj = JSON.parse($response.body);
 if (obj.aweme_list) {
   for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
     if (obj.aweme_list[i].raw_ad_data ||
@@ -28,23 +17,5 @@ if (obj.aweme_list) {
     obj.aweme_list[i].status.reviewed = 1;
     obj.aweme_list[i].video_control = arr;
   }
-  $done({JSON.stringify(obj)});
-} else if (obj.data) {
-  for (var i = obj.data.length - 1; i >= 0; i--) {
-    if (obj.data[i].aweme) {
-      if (obj.data[i].aweme.poi_info) {
-        delete obj.data[i].aweme.poi_info;
-      }
-      if (obj.data[i].aweme.simple_promotions) {
-        delete obj.data[i].aweme.simple_promotions;
-      }
-      obj.data[i].aweme.status.reviewed = 1;
-      obj.data[i].aweme.video_control = arr;
-    } else {
-      obj.data.splice(i, 1);
-    }
-  }
-  $done({JSON.stringify(obj)});
-} else {
-  $done({body});
 }
+$done(JSON.stringify(obj));
