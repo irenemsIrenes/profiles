502c342ea640a05099d26afccc457615e2f889e3
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-13 19:48:12 +0800

    Create xigua.js

---
+++ Quantumult/X/js/xigua.js
@@ -0,0 +1,11 @@
+var obj = JSON.parse($response.body);
+if (obj.data) {
+  for (var i = obj.data.length - 1; i >= 0; i--) {
+    let content = JSON.parse(obj.data[i].content);
+    if (content.raw_ad_data || content.label == '广告' || content.ad_id
+    	|| content.ad_label)
+    	obj.data.splice(i, 1);
+      $notify("xi gua", "ads", "remvoved");
+  }
+}
+$done(JSON.stringify(obj));
