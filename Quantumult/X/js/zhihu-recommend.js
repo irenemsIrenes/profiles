67a3a0e9e1ba80599e1bbb66a4363d32fe69fd44
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-23 07:52:57 +0800

    Update zhihu-recommend.js

--- Quantumult/X/js/zhihu-recommend.js
+++ Quantumult/X/js/zhihu-recommend.js
@@ -1,3 +1,9 @@
+/**
+ * @supported 00D3992C8F27
+ * The above random generated device ID can be found at the bottom of Quantumult X additional menu, and may be changed when system restored.
+ * Indicate what device are supported by the file. This is necessary when the file is not loaded from local("On My iPhone - Quantumult X - Scripts").
+ */
+
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
