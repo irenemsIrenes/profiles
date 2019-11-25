5d8e2a720722709b1652557d7381ade1e9440b40
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-25 14:10:09 +0800

    Create weibo-mobile.js

---
+++ Quantumult/X/js/weibo-mobile.js
@@ -0,0 +1,16 @@
+/**
+ * @supported 00D3992C8F27
+ */
+
+
+var obj = JSON.parse($response.body);
+if (obj.data && obj.data.cards) {
+  for (var i = obj.data.cards.length - 1; i >= 0; i--) {
+      let card = obj.data.cards[i];
+      if (card.uve && card.uve.type == "ad") {
+          obj.data.cards.splice(i, 1);
+          $notify("weibo", "ads", "remvoved");
+      }
+  }
+}
+$done(JSON.stringify(obj));
