3d8716ea0ba61b0f8b1c137e8e1f1fa1077d9cb4
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-25 13:35:16 +0800

    Update zhihu-recommend.js

--- Quantumult/X/js/zhihu-recommend.js
+++ Quantumult/X/js/zhihu-recommend.js
@@ -2,13 +2,12 @@
  * @supported 00D3992C8F27
  */
 
-$notify("zhihu", "ads", "remvoved");
-
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
     if(element['id'].startsWith('AD_')){      
-       body['data'].splice(index,1)  
+       body['data'].splice(index,1)
+       $notify("zhihu", "ads", "remvoved");
     }
 })
 body=JSON.stringify(body)
