3070760fccab5fcb4f7e14680b73d52c87dc95a5
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-27 17:24:16 +0800

    Update zhihu-recommend.js

--- Quantumult/X/js/zhihu-recommend.js
+++ Quantumult/X/js/zhihu-recommend.js
@@ -6,9 +6,8 @@ let body = $response.body
 body = JSON.parse(body)
 let cnt = 0
 body['data'].forEach((element, index)=> {
-    let footer = element['footer']
 
-    if(element['id'].startsWith('AD_') || (footer && ["Live 讲座","电子书"].indexOf(footer['label']) != -1) ) {      
+    if(element['id'].startsWith('AD_') || element['card_type'] == 'slot_event_card' ) {      
        body['data'].splice(index,1)
        ++cnt
     }
