487dc5799a18771a23efaa5586a26d1373a7ea69
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-11-29 17:25:04 +0800

    Update douyin-aweme.js

--- Quantumult/X/js/douyin-aweme.js
+++ Quantumult/X/js/douyin-aweme.js
@@ -7,8 +7,7 @@ $notify("douyin", "ads", "not remvoved");
 var obj = JSON.parse($response.body);
 if (obj.aweme_list) {
   for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
-    if (obj.aweme_list[i].raw_ad_data ||
-          obj.aweme_list[i].is_ads == true) {
+    if (obj.aweme_list[i].is_ads == true) {
       obj.aweme_list.splice(i, 1);
       $notify("douyin", "ads", "remvoved");
     }
