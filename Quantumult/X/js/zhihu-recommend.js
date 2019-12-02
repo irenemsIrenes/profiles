a0fdc9fad49275b9edbfc89c68b3b6ab368eae95
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-12-02 16:40:11 +0800

    Update zhihu-recommend.js

--- Quantumult/X/js/zhihu-recommend.js
+++ Quantumult/X/js/zhihu-recommend.js
@@ -11,6 +11,16 @@ for (let i = body.data.length - 1; i >= 0; --i) {
     if(e['id'].startsWith('AD_') || e['card_type'] == 'slot_event_card' ) {      
        body.data.splice(i,1)
        ++cnt
+    } else if (e.common_card && e.common_card.feed_content && e.common_card.feed_content.source_line
+              && e.common_card.feed_content.source_line.elements) {
+       let elements = e.common_card.feed_content.source_line.elements
+       for (let k = 0; k < elements.length; ++k) {
+           let ee = elements[i]
+           if (ee.text && ee.text.panel_text.indexOf('盐选推荐') >= 0) {
+               body.data.splice(i,1)
+               ++cnt
+           }
+       }
     }
 }
 
