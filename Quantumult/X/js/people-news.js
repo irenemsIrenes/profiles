3f877e58c82a8cbd965ce2951bc09be5f23de621
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2019-12-18 09:26:10 +0800

    Update people-news.js

--- Quantumult/X/js/people-news.js
+++ Quantumult/X/js/people-news.js
@@ -5,12 +5,14 @@
 
 var obj = JSON.parse($response.body);
 if (obj.data) {
+let cnt = 0
   for (var i = obj.data.length - 1; i >= 0; i--) {
       let item = obj.data[i];
-      if (item.view_type == 'advert') {
+      if (item.view_type && item.view_type.startsWith('advert')) {
           obj.data.splice(i, 1);
-          $notify("people news", "ads", "remvoved");
+          ++cnt;
       }
   }
+  $notify("people news", "ads", "remvoved " + cnt);
 }
 $done(JSON.stringify(obj));
