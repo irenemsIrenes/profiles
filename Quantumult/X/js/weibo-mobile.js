686b6561403c37c7ae5f4d659657af80fa8c1b5e
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2020-05-07 08:49:29 +0800

    Update weibo-mobile.js

--- Quantumult/X/js/weibo-mobile.js
+++ Quantumult/X/js/weibo-mobile.js
@@ -1,7 +1,7 @@
 /**
  * @supported 00D3992C8F27 8B87B7345981
  */
-const userPattern = /.*(娱乐|肖战|圈内).*/;
+const userPattern = /.*(娱乐|肖战|圈内|整形).*/;
 const textPattern = /.*(肖战).*/;
 const verifiedPattern = /.*(娱乐|综艺|电视团|剧评人|电视剧|超话|职业投资|营养师|美妆).*/;
 const fromPattern = /.*(超话).*/;
