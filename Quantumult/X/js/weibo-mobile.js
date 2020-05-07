6c2695d3ee7e01c3367ec833c2b3e7496064dabe
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2020-05-07 08:37:06 +0800

    Update weibo-mobile.js

--- Quantumult/X/js/weibo-mobile.js
+++ Quantumult/X/js/weibo-mobile.js
@@ -1,9 +1,10 @@
 /**
  * @supported 00D3992C8F27 8B87B7345981
  */
-const userPattern = /.*(娱乐|肖战).*/;
+const userPattern = /.*(娱乐|肖战|圈内).*/;
 const textPattern = /.*(肖战).*/;
-const verifiedPattern = /.*(娱乐|综艺|电视团|剧评人|电视剧|超话|职业投资).*/;
+const verifiedPattern = /.*(娱乐|综艺|电视团|剧评人|电视剧|超话|职业投资|营养师).*/;
+const fromPattern = /.*(超话).*/;
 
 
 var obj = JSON.parse($response.body);
@@ -13,7 +14,8 @@ if (obj.data && obj.data.cards) {
       if (card.uve && card.uve.type == "ad") {
           obj.data.cards.splice(i, 1);
       } else if(card.mblog) {
-        if(card.mblog.user && ( verifiedPattern.test(card.mblog.user.verified_reason) || userPattern.test(card.mblog.user.screen_name))) {
+        if(card.mblog.user && ( verifiedPattern.test(card.mblog.user.verified_reason) || userPattern.test(card.mblog.user.screen_name)
+                              || fromPattern.test(card.mblog.user.source))) {
            obj.data.cards.splice(i, 1);
         } else if (textPattern.test(card.mblog.text)) {
           obj.data.cards.splice(i, 1);
