5748297ef37cfa706cdfc24c920f8d72771ad23d
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2020-05-30 11:46:24 +0800

    Update weibo-mobile.js

--- Quantumult/X/js/weibo-mobile.js
+++ Quantumult/X/js/weibo-mobile.js
@@ -3,7 +3,7 @@
  */
 const userPattern = /.*(娱乐|肖战|圈内|整形).*/;
 const textPattern = /.*(肖战).*/;
-const verifiedPattern = /.*(娱乐|综艺|电视团|剧评人|电视剧|超话|职业投资|营养师|美妆|知名).*/;
+const verifiedPattern = /.*(娱乐|综艺|电视团|剧评人|电视剧|超话|职业投资|营养师|美妆).*/;
 const fromPattern = /.*(超话).*/;
 
 
@@ -14,7 +14,10 @@ if (obj.data && obj.data.cards) {
       if (card.uve && card.uve.type == "ad") {
           obj.data.cards.splice(i, 1);
       } else if(card.mblog) {
-        if(card.mblog.user && ( verifiedPattern.test(card.mblog.user.verified_reason) || userPattern.test(card.mblog.user.screen_name)
+        if (card.mblog.is_vote == 1) {
+          obj.data.cards.splice(i, 1);
+        }
+        eles if(card.mblog.user && ( verifiedPattern.test(card.mblog.user.verified_reason) || userPattern.test(card.mblog.user.screen_name)
                               )) {
            obj.data.cards.splice(i, 1);
         } else if (textPattern.test(card.mblog.text) || fromPattern.test(card.mblog.source)) {
