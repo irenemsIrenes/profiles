e0f34b5f32bc0a5388adc5dcac6578345d4bd3ec
irenemsIrenes <56461012+irenemsIrenes@users.noreply.github.com>
2021-03-24 10:35:06 +0800

    Update weibo-mobile.js

--- Quantumult/X/js/weibo-mobile.js
+++ Quantumult/X/js/weibo-mobile.js
@@ -5,6 +5,7 @@ const userPattern = /.*(娱乐|肖战|圈内|整形).*/;
 const textPattern = /.*(肖战).*/;
 const verifiedPattern = /.*(娱乐|综艺|股评|知名情感博主|电视团|剧评人|电视剧|超话|职业投资|营养师|美妆|时尚|运动|体育|健身|好物发现).*/;
 const fromPattern = /.*(超话).*/;
+const userDescPattern = /娱乐|电影/
 
 
 var obj = JSON.parse($response.body);
@@ -18,7 +19,7 @@ if (obj.data && obj.data.cards) {
           obj.data.cards.splice(i, 1);
         }
         else if(card.mblog.user && ( verifiedPattern.test(card.mblog.user.verified_reason) || userPattern.test(card.mblog.user.screen_name)
-                              )) {
+                              || userDescPattern.test(card.mblog.user.description)) {
            obj.data.cards.splice(i, 1);
         } else if (textPattern.test(card.mblog.text) || fromPattern.test(card.mblog.source)) {
           obj.data.cards.splice(i, 1);
