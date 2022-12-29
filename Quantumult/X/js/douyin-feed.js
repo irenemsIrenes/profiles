7156773d54b20f76bba8df558c2caf163afcfc3d
Jun Zhao <1940295+zhaojunlucky@users.noreply.github.com>
2022-12-29 11:07:17 +0800

    update douyin

--- Quantumult/X/js/douyin-feed.js
+++ Quantumult/X/js/douyin-feed.js
@@ -1,1 +1,8 @@
-$done({ url: $request.url.replace(/^https:\/\/.*\.amemv\.com\/aweme\/v\d\//, "https://aweme.snssdk.com/aweme/v1/") });
+const urlPattern = new RegExp("^https:\/\/api.*\.amemv\.com\/aweme\/v\d\/feed\/.+", 'mi')
+
+if (urlPattern.test($request.url)) {
+	$done({ url: $request.url.replace(/^https:\/\/.*\.amemv\.com\/aweme\/v\d\//, "https://aweme.snssdk.com/aweme/v1/") });
+
+} else {
+	$done({})
+}
