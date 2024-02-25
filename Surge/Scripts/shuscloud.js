3114d8c294d9d8cea8ce53151763062a2c3fa171
magicworldz <1940295+zhaojunlucky@users.noreply.github.com>
2024-02-25 17:26:59 +0800

    shuscloud info

---
+++ Surge/Scripts/shuscloud.js
@@ -0,0 +1,26 @@
+const url = $persistentStore.read("shuscloudurl")
+
+if (url == null) {
+    $notification.post("Shuscloud", "url is not set", "")
+} else {
+    $httpClient.get(url, function (error, response, data) {
+
+       if (error != null) {
+           console.log(error)
+           console.log(response)
+           console.log(data)
+           $notification.post("Shuscloud", JSON.stringify(error), "")
+       } else {
+           const info = JSON.parse(data)
+           if (!info.success) {
+               $notification.post("Shuscloud", data, "")
+           } else {
+               const used = info.data.traffic.limit*info.data.traffic.used
+               $notification.post("Shuscloud", `${new Date().toLocaleString()} Total ${info.data.traffic.limit} GB, used ${used.toPrecision(2)} GB`, "")
+           }
+
+       }
+       $done();
+    });
+}
+
