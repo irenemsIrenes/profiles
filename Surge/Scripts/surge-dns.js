62521b233b79d1770fe6393ab06ab863c6bf93a2
magicworldz <1940295+zhaojunlucky@users.noreply.github.com>
2024-05-24 20:04:49 +0800

    update dns js

--- Surge/Scripts/surge-dns.js
+++ Surge/Scripts/surge-dns.js
@@ -1,8 +1,8 @@
-let preferIPv6 = false
-if ($network.v6 && $network.v6.primaryAddress) {
-    console.log("will use IPv6")
-    preferIPv6 = true
-}
+// let preferIPv6 = false
+// if ($network.v6 && $network.v6.primaryAddress) {
+//     console.log("will use IPv6")
+//     preferIPv6 = true
+// }
 
 const dnsData = $persistentStore.read("dns")
 if (!dnsData) {
@@ -11,14 +11,15 @@ if (!dnsData) {
 } else {
     const dns = JSON.parse(dnsData)
     if (dns.hasOwnProperty($domain)) {
+        ipaddrs = []
         const domainDns = dns[$domain]
-        if (preferIPv6 && domainDns.hasOwnProperty("ipv6") && domainDns.ipv6) {
-            console.log("domain " + $domain + " use ipv6")
-            $done({addresses: domainDns.ipv6, ttl: 600});
-        } else {
-            console.log("domain " + $domain + " use ipv4")
-            $done({addresses: domainDns.ipv4, ttl: 600});
+        if (domainDns.hasOwnProperty("ipv6") && domainDns.ipv6) {
+            ipaddrs = ipaddrs.concat(domainDns.ipv6)
         }
+        if (domainDns.hasOwnProperty("ipv4") && domainDns.ipv4) {
+            ipaddrs = ipaddrs.concat(domainDns.ipv4)
+        }
+        $done({addresses: ipaddrs, ttl: 600});
     } else {
         console.log("domain " + $domain + " is not found in local dns")
         $done({})
